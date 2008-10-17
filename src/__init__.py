c01769fa7c0b89b0e21f87b4085ea98bd934ffa1
Christopher O'Brien <obriencj@gmail.com>
2008-10-17 21:48:47 +0000

    let's get classy, since some of these things need references back to an owning constant pool

--- src/__init__.py
+++ src/__init__.py
@@ -15,32 +15,22 @@ author: Christopher O'Brien  <siege@preoccupied.net>
 
 
 
-class JavaClassInfo(object):
+class JavaConstantPool(object):
+
     def __init__(self):
-        self.magic = 0
-        self.version = (0,0)
         self.consts = tuple()
-        self.interfaces = tuple()
-        self.fields = tuple()
-        self.methods = tuple()
-        self.attributes = {}
-        self.attr_map = None
 
-        self.sourcefile_ref = 0
+
+    def funpack(self, buff):
+        (count,), buff = _funpack(">H", buff)
+        items = [None,]
     
-
-    def pretty_const(self, i):
-        t,v = pretty_type_val(*self.consts[i])
-        return "const #%i = %s\t%s;" % (i, t, v)
-
-
-    def pretty(self):
-        pass
+        for i in xrange(1, count):
+            item, buff = _funpack_const_item(buff)
+            items.append(item)
         
-
-    def print_consts(self):
-        for i in xrange(1, len(self.consts)):
-            print self.pretty_const(i)
+        self.consts = tuple(items)
+        return buff
 
 
     def get_const(self, index):
@@ -66,9 +56,78 @@ class JavaClassInfo(object):
             return tuple([self.get_const_val(i) for i in v])
 
 
+    def pretty_const(self, index):
+        type,val = self.consts[index]
+
+        if type == 1:
+            type = "Asciz"
+            val = str(val)
+        elif type == 3:
+            type = "Integer"
+        elif type == 4:
+            type = "Float"
+        elif type == 5:
+            type = "Long"
+        elif type == 6:
+            type = "Double"
+        elif type == 7:
+            type = "class"
+            val = "#%i" % val
+        elif type == 8:
+            type = "String"
+            val = "#%i" % val
+        elif type == 9:
+            type = "Field"
+            val = "#%i.#%i" % val
+        elif type == 10:
+            type = "Method"
+            val = "#%i.#%i" % val
+        elif type == 11:
+            type = "InterfaceMethod"
+            val = "#%i.#%i" % val
+        elif type == 12:
+            type = "NameAndType"
+            val = "#%i:#%i" % val
+
+        return "const #%i = %s\t%s;" % (index, type, val)
+
+
+    def print_consts(self):
+        for i in xrange(1, len(self.consts)):
+            print self.pretty_const(i)
+
+
+
+class JavaAttributes(object):
+
+    def __init__(self, owner=None):
+        self.attributes = tuple()
+        self.attr_map = None
+        
+        if not owner and isinstance(self, JavaConstantPool):
+            owner = self
+
+        self.owner = owner
+
+
+    def funpack(self, buff):
+        (count,), buff = _funpack(">H", buff)
+        items = []
+
+        for i in xrange(0, count):
+            (name, size,), buff = _funpack(">HI", buff)
+            data = buffer(buff, 0, size)
+            buff = buffer(buff, size)
+            items.append( (name, data) )
+
+        self.attributes = tuple(items)
+        return buff
+
+
     def get_attributes_as_map(self):
+        o = self.owner
         if self.attr_map is None:
-            pairs = ((self.get_const_val(i),v) for (i,v) in self.attributes)
+            pairs = ((o.get_const_val(i),v) for (i,v) in self.attributes)
             self.attr_map = dict(pairs)
 
         return self.attr_map
@@ -76,6 +135,37 @@ class JavaClassInfo(object):
 
     def get_attribute(self, name):
         return self.get_attributes_as_map()[name]
+
+
+
+class JavaClassInfo(JavaConstantPool, JavaAttributes):
+    def __init__(self):
+        JavaConstantPool.__init__(self)
+        JavaAttributes.__init__(self)
+
+        self.magic = 0
+        self.version = (0,0)
+        self.interfaces = tuple()
+        self.fields = tuple()
+        self.methods = tuple()
+
+        self.sourcefile_ref = 0
+
+
+    def funpack(self, buff):
+
+        self.magic, buff = _funpack("BBBB", buff)
+        self.version, buff = _funpack(">HH", buff)
+        buff = JavaConstantPool.funpack(self, buff)
+        (self.access_flags,), buff = _funpack(">H", buff)
+        (self.this_ref,), buff = _funpack(">H", buff)
+        (self.super_ref,), buff = _funpack(">H", buff)
+        self.interfaces, buff = _funpack_interfaces(buff)
+        self.fields, buff = _funpack_members(buff, owner=self)
+        self.methods, buff = _funpack_members(buff, owner=self)
+        buff = JavaAttributes.funpack(self, buff)
+        
+        return buff
 
 
     def get_sourcefile(self):
@@ -96,47 +186,36 @@ class JavaClassInfo(object):
 
 
 
-class JavaMemberInfo(object):
-    def __init__(self):
+class JavaMemberInfo(JavaAttributes):
+    def __init__(self, owner):
+        JavaAttributes.__init__(self, owner)
+
         self.access_flags = 0
         self.name_ref = 0
         self.descriptor_ref = 0
-        self.attributes = tuple()
-
-        self.owner = None
-        self.attr_map = None
 
 
-    def pretty(self):
-        pass
+    def funpack(self, buff):
+        (a, b, c), buff = _funpack(">HHH", buff)
+
+        self.access_flags = a
+        self.name_ref = b
+        self.descriptor_ref = c
+        buff = JavaAttributes.funpack(self, buff)
+
+        return buff
 
 
     def get_name(self):
         if not self.owner:
-            raise Exception("member has no owner")
+            raise Exception("member has no owning class")
         return self.owner.get_const_val(self.name_ref)
 
 
     def get_descriptor(self):
         if not self.owner:
-            raise Exception("memeber has no owner")
+            raise Exception("memeber has no owning class")
         return self.owner.get_const_val(self.descriptor_ref)
-
-
-    def get_attributes_as_map(self):
-        if not self.owner:
-            raise Exception("memeber has no owner")
-
-        if self.attr_map is None:
-            o = self.owner
-            pairs = ((o.get_const_val(i),v) for (i,v) in self.attributes)
-            self.attr_map = dict(pairs)
-
-        return self.attr_map
-
-
-    def get_attribute(self, name):
-        return self.get_attributes_as_map().get(name)
 
 
     def get_code(self):
@@ -167,38 +246,18 @@ class JavaMemberInfo(object):
 
 
 
-def pretty_type_val(type, val):
-    if type == 1:
-        type = "Asciz"
-        val = str(val)
-    elif type == 3:
-        type = "Integer"
-    elif type == 4:
-        type = "Float"
-    elif type == 5:
-        type = "Long"
-    elif type == 6:
-        type = "Double"
-    elif type == 7:
-        type = "class"
-        val = "#%i" % val
-    elif type == 8:
-        type = "String"
-        val = "#%i" % val
-    elif type == 9:
-        type = "Field"
-        val = "#%i.#%i" % val
-    elif type == 10:
-        type = "Method"
-        val = "#%i.#%i" % val
-    elif type == 11:
-        type = "InterfaceMethod"
-        val = "#%i.#%i" % val
-    elif type == 12:
-        type = "NameAndType"
-        val = "#%i:#%i" % val
+class JavaCodeInfo(JavaAttributes):
+    def __init__(self, owner):
+        JavaAttributes.__init__(self, owner)
 
-    return (type, val)
+        self.max_stack = 0
+        self.max_locals = 0
+        self.code = None
+        self.exceptions = tuple()
+
+
+    def funpack(self, buff):
+        return buff
 
 
 
@@ -290,18 +349,6 @@ def _funpack_const_item(buff):
 
 
 
-def _funpack_consts(buff):
-    (count,), buff = _funpack(">H", buff)
-    items = [None,]
-    
-    for i in xrange(1, count):
-        item, buff = _funpack_const_item(buff)
-        items.append(item)
-
-    return tuple(items), buff
-
-
-
 def _funpack_interfaces(buff):
     (count,), buff = _funpack(">H", buff)
     items = []
@@ -314,64 +361,31 @@ def _funpack_interfaces(buff):
 
 
 
-def _funpack_attributes(buff):
-    (count,), buff = _funpack(">H", buff)
-    items = []
-
-    for i in xrange(0, count):
-        (name, size,), buff = _funpack(">HI", buff)
-        data = buffer(buff, 0, size)
-        buff = buffer(buff, size)
-        items.append( (name, data) )
-
-    return tuple(items), buff
-
-
-
-def _funpack_member_info(buff):
-    f = JavaMemberInfo()
-
-    (a, b, c), buff = _funpack(">HHH", buff)
-    f.access_flags = a
-    f.name_ref = b
-    f.descriptor_ref = c
-    f.attributes, buff = _funpack_attributes(buff)
-
-    return f, buff
-
-
-
 def _funpack_members(buff, owner=None):
     (count,), buff = _funpack(">H", buff)
     items = []
 
     for i in xrange(0, count):
-        field, buff = _funpack_member_info(buff)
-        field.owner = owner
-        items.append(field)
+        member = JavaMemberInfo(owner)
+        buff = member.funpack(buff)
+        items.append(member)
 
     return tuple(items), buff
 
 
 
-def unpack_class(buff):
+def funpack_class(buff):
     if not is_class(buff):
         raise Exception("not a Java class file")
 
     o = JavaClassInfo()
+    return o, o.funpack(buff)
 
-    o.magic, buff = _funpack("BBBB", buff)
-    o.version, buff = _funpack(">HH", buff)
-    o.consts, buff = _funpack_consts(buff)
-    (o.access_flags,), buff = _funpack(">H", buff)
-    (o.this_ref,), buff = _funpack(">H", buff)
-    (o.super_ref,), buff = _funpack(">H", buff)
-    o.interfaces, buff = _funpack_interfaces(buff)
-    o.fields, buff = _funpack_members(buff, owner=o)
-    o.methods, buff = _funpack_members(buff, owner=o)
-    o.attributes, buff = _funpack_attributes(buff)
 
-    return o
+
+def unpack_class(buff):
+    info, buff = funpack_class(buff)
+    return info
 
 
 
