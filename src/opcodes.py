9e0d99b725ec3168a2f56fda8504943f4f25cecb
Christopher O'Brien <obriencj@gmail.com>
2008-11-06 14:22:12 +0000

    opcodes modules seems to be working now

--- src/opcodes.py
+++ src/opcodes.py
@@ -10,8 +10,10 @@ _optable = {}
 
 
 
-def _op(name,val,argc=0,consume=0,produce=0):
-    operand = (name,val,argc,consume,produce)
+def _op(name,val,format=None,consume=0,produce=0):
+    name = name.lower()
+
+    operand = (name,val,format,consume,produce)
 
     assert(not _optable.has_key(name))
     assert(not _optable.has_key(val))
@@ -22,18 +24,90 @@ def _op(name,val,argc=0,consume=0,produce=0):
 
 
 
+def get_opcode_by_name(name):
+    return _optable[name.lower()][1]
+
+
+
+def get_opname_by_code(code):
+    return _optable[code][0]
+
+
+
+def get_arg_format(code):
+    return _optable[code][2]
+
+
+
+def _unpack(frmt, bc, offset=0):
+    from javaclass import _compile
+
+    sfmt = _compile(frmt)
+
+    buff = buffer(bc, offset, sfmt.size)
+    
+    if len(bc) < sfmt.size:
+        raise Exception("not enough data in buffer for format")
+
+    val = sfmt.unpack(buff)
+    return val, offset+sfmt.size
+
+
+
+def _unpack_lookupswitch(bc, offset):
+    pass
+
+
+
+def _unpack_tableswitch(bc, offset):
+    pass
+
+
+
+def _unpack_wide(bc, offset):
+    pass
+
+
+
+def disassemble(bytecode):
+    
+    offset = 0
+    end = len(bytecode)
+
+    dis = []
+
+    while offset < end:
+        orig_offset = offset
+
+        code = bytecode[offset]
+        offset += 1
+
+        format = get_arg_format(code)
+        if callable(format):
+            args,offset = format(bytecode, offset)
+        elif format:
+            args,offset = _unpack(format, bytecode, offset)
+
+        dis.append( (code, args, orig_offset) )
+
+    return tuple(dis)
+
+
+
+# And now for all the operator definitions
+
 _op("aaload", 0x32)
 _op("aastore", 0x53)
 _op("aconst_null", 0x01)
-_op("aload", 0x19, format=1)
+_op("aload", 0x19, format=">B")
 _op("aload_0", 0x2a)
 _op("aload_1", 0x2b)
 _op("aload_2", 0x2c)
 _op("aload_3", 0x2d)
-_op("anewarray", 0xbd, argc=2)
+_op("anewarray", 0xbd, format=">H")
 _op("areturn", 0xb0)
 _op("arraylength", 0xbe)
-_op("astore", 0x3a, argc=1)
+_op("astore", 0x3a, format=">B")
 _op("astore_0", 0x4b)
 _op("astore_1", 0x4c)
 _op("astore_2", 0x4d)
@@ -42,11 +116,11 @@ _op("athrow", 0xbf)
 
 _op("baload", 0x33)
 _op("bastore", 0x54)
-_op("bipush", 0x10, argc=1)
+_op("bipush", 0x10, format=">B")
 
 _op("caload", 0x34)
 _op("castore", 0x55)
-_op("checkcast", 0xc0, argc=2)
+_op("checkcast", 0xc0, format=">H")
 
 _op("d2f", 0x90)
 _op("d2i", 0x8e)
@@ -59,7 +133,7 @@ _op("dcmpl", 0x97)
 _op("dconst_0", 0x0e)
 _op("dconst_1", 0x0f)
 _op("ddiv", 0x6f)
-_op("dload", 0x18, argc=1)
+_op("dload", 0x18, format=">B")
 _op("dload_0", 0x26)
 _op("dload_1", 0x27)
 _op("dload_2", 0x28)
@@ -68,7 +142,7 @@ _op("dmul", 0x6b)
 _op("dneg", 0x77)
 _op("drem", 0x73)
 _op("dreturn", 0xaf)
-_op("dstore", 0x39, argc=1)
+_op("dstore", 0x39, format=">B")
 _op("dstore_0", 0x47)
 _op("dstore_1", 0x48)
 _op("dstore_2", 0x49)
@@ -93,7 +167,7 @@ _op("fconst_0", 0x0b)
 _op("fconst_1", 0x0c)
 _op("fconst_2", 0x0d)
 _op("fdiv", 0x6e)
-_op("fload", 0x17, argc=1)
+_op("fload", 0x17, format=">B")
 _op("fload_0", 0x22)
 _op("fload_1", 0x23)
 _op("fload_2", 0x24)
@@ -102,17 +176,17 @@ _op("fmul", 0x6a)
 _op("fneg", 0x76)
 _op("frem", 0x72)
 _op("freturn", 0x174)
-_op("fstore", 0x38, argc=1)
+_op("fstore", 0x38, format=">B")
 _op("fstore_0", 0x43)
 _op("fstore_1", 0x44)
 _op("fstore_2", 0x45)
 _op("fstore_3", 0x46)
 _op("fsub", 0x66)
 
-_op("getfield", 0xb4, argc=2)
-_op("getstatic", 0xb2, argc=2)
-_op("goto", 0xa7, argc=2)
-_op("goto_w", 0xc8, argc=4)
+_op("getfield", 0xb4, format=">H")
+_op("getstatic", 0xb2, format=">H")
+_op("goto", 0xa7, format=">h")
+_op("goto_w", 0xc8, format=">i")
 
 _op("i2b", 0x91)
 _op("i2c", 0x92)
@@ -132,41 +206,41 @@ _op("iconst_3", 0x06)
 _op("iconst_4", 0x07)
 _op("iconst_5", 0x08)
 _op("idiv", 0x6c)
-_op("if_acmpeq", 0xa5, argc=2)
-_op("if_acmpne", 0xa6, argc=2)
-_op("if_icmpeq", 0x9f, argc=2)
-_op("if_icmpne", 0xa0, argc=2)
-_op("if_icmplt", 0xa1, argc=2)
-_op("if_icmpge", 0xa2, argc=2)
-_op("if_icmpgt", 0xa3, argc=2)
-_op("if_icmple", 0xa4, argc=2)
-_op("ifeq", 0x99, argc=2)
-_op("ifne", 0x9a, argc=2)
-_op("iflt", 0x9b, argc=2)
-_op("ifge", 0x9c, argc=2)
-_op("ifgt", 0x9d, argc=2)
-_op("ifle", 0x9e, argc=2)
-_op("ifnonnull", 0xc7, argc=2)
-_op("ifnull", 0xc6, argc=2)
-_op("iinc", 0x84, argc=2)
-_op("iload", 0x15, argc=1)
+_op("if_acmpeq", 0xa5, format=">h")
+_op("if_acmpne", 0xa6, format=">h")
+_op("if_icmpeq", 0x9f, format=">h")
+_op("if_icmpne", 0xa0, format=">h")
+_op("if_icmplt", 0xa1, format=">h")
+_op("if_icmpge", 0xa2, format=">h")
+_op("if_icmpgt", 0xa3, format=">h")
+_op("if_icmple", 0xa4, format=">h")
+_op("ifeq", 0x99, format=">h")
+_op("ifne", 0x9a, format=">h")
+_op("iflt", 0x9b, format=">h")
+_op("ifge", 0x9c, format=">h")
+_op("ifgt", 0x9d, format=">h")
+_op("ifle", 0x9e, format=">h")
+_op("ifnonnull", 0xc7, format=">h")
+_op("ifnull", 0xc6, format=">h")
+_op("iinc", 0x84, format=">Bb")
+_op("iload", 0x15, format=">B")
 _op("iload_0", 0x1a)
 _op("iload_1", 0x1b)
 _op("iload_2", 0x1c)
 _op("iload_3", 0x1d)
 _op("imul", 0x68)
 _op("ineg", 0x74)
-_op("instanceof", 0xc1, argc=2)
-_op("invokeinterface", 0xb9, argc=4)
-_op("invokespecial", 0xb7, argc=2)
-_op("invokestatic", 0xb8, argc=2)
-_op("invokevirtual", 0xb6, argc=2)
+_op("instanceof", 0xc1, format=">H")
+_op("invokeinterface", 0xb9, format=">HBB")
+_op("invokespecial", 0xb7, format=">H")
+_op("invokestatic", 0xb8, format=">H")
+_op("invokevirtual", 0xb6, format=">H")
 _op("ior", 0x80)
 _op("irem", 0x70)
 _op("ireturn", 0xac)
 _op("ishl", 0x78)
 _op("ishr", 0x7a)
-_op("istore", 0x36, argc=1)
+_op("istore", 0x36, format=">B")
 _op("istore_0", 0x3b)
 _op("istore_1", 0x3c)
 _op("istore_2", 0x3d)
@@ -175,8 +249,8 @@ _op("isub", 0x64)
 _op("iushr", 0x7c)
 _op("ixor", 0x82)
 
-_op("jsr", 0xa8, argc=2)
-_op("jsr_w", 0xc9, argc=4)
+_op("jsr", 0xa8, format=">h")
+_op("jsr_w", 0xc9, format=">i")
 
 _op("l2d", 0x8a)
 _op("l2f", 0x89)
@@ -188,24 +262,24 @@ _op("lastore", 0x50)
 _op("lcmp", 0x94)
 _op("lconst_0", 0x09)
 _op("lconst_1", 0x0a)
-_op("ldc", 0x12, argc=1)
-_op("ldc_w", 0x13, argc=2)
-_op("ldc2_w", 0x14, argc=2)
+_op("ldc", 0x12, format=">B")
+_op("ldc_w", 0x13, format=">H")
+_op("ldc2_w", 0x14, format=">H")
 _op("ldiv", 0x6d)
-_op("lload", 0x16, argc=1)
+_op("lload", 0x16, format=">B")
 _op("lload_0", 0x1e)
 _op("lload_1", 0x1f)
 _op("lload_2", 0x20)
 _op("lload_3", 0x21)
 _op("lmul", 0x69)
 _op("lneg", 0x75)
-_op("lookupswitch", 0xab, argc=-1)
+_op("lookupswitch", 0xab, format=_unpack_lookupswitch)
 _op("lor", 0x81)
 _op("lrem", 0x71)
 _op("lreturn", 0xad)
 _op("lshl", 0x79)
 _op("lshr", 0x7b)
-_op("lstore", 0x37, argc=1)
+_op("lstore", 0x37, format=">B")
 _op("lstore_0", 0x3f)
 _op("lstore_1", 0x40)
 _op("lstore_2", 0x41)
@@ -218,41 +292,26 @@ _op("monitorentry", 0xc2)
 _op("monitorexit", 0xc3)
 _op("multianewarray", 0xc5)
 
-_op("new", 0xbb, argc=2)
-_op("newarray", 0xbc, argc=1)
+_op("new", 0xbb, format=">H")
+_op("newarray", 0xbc, format=">B")
 _op("nop", 0x00)
 
 _op("pop", 0x57)
 _op("pop2", 0x58)
-_op("putfield", 0xb5, argc=2)
-_op("putstatic", 0xb3, argc=2)
+_op("putfield", 0xb5, format=">H")
+_op("putstatic", 0xb3, format=">H")
 
-_op("ret", 0xa9, argc=1)
+_op("ret", 0xa9, format=">B")
 _op("return", 0xb1)
 
 _op("saload", 0x35)
 _op("sastore", 0x56)
-_op("sipush", 0x11, argc=2)
+_op("sipush", 0x11, format=">h")
 _op("swap", 0x5f)
 
-_op("tableswitch", 0xaa, argc=-1)
+_op("tableswitch", 0xaa, format=_unpack_tableswitch)
 
-_op("wide", 0xc4, argc=-1)
-
-
-
-def get_opcode_by_name(name):
-    return _optable[name.lowercase()][1]
-
-
-
-def get_opname_by_code(code):
-    return _optable[code][0]
-
-
-
-def get_argcount(code):
-    return _optable[code][2]
+_op("wide", 0xc4, format=_unpack_wide)
 
 
 
