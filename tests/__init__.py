ec77edb0af5ad3673d95a20652b435e2d9e4eb59
Christopher O'Brien <obriencj@gmail.com>
2014-04-01 18:13:44 -0400

    Sample1 const pool, method code, line number tables

--- tests/__init__.py
+++ tests/__init__.py
@@ -24,6 +24,7 @@ license: LGPL v.3
 from unittest import TestCase
 
 import javatools as jt
+import javatools.opcodes as op
 import pkg_resources
 
 
@@ -38,19 +39,15 @@ def load(which):
     return jt.unpack_classfile(fn)
 
 
-class ClassfileTest(TestCase):
+class Sample1Tests(TestCase):
 
 
     def test_is_class_file(self):
         fn = get_class_fn("Sample1")
-
-        with open(fn, "rb") as fd:
-            magic = fd.read(4)
-
         self.assertTrue(jt.is_class_file(fn))
 
 
-    def test_sample1_classinfo(self):
+    def test_classinfo(self):
         ci = load("Sample1")
 
         self.assertEqual(type(ci), jt.JavaClassInfo)
@@ -93,7 +90,47 @@ class ClassfileTest(TestCase):
         self.assertEqual(ci.get_invisible_annotations(), tuple())
 
 
-    def test_sample1_field_name(self):
+    def test_const_pool(self):
+        ci = load("Sample1")
+
+        exp = ( (0, (None, None)),
+                (1, (jt.CONST_String, 24)),
+                (2, (jt.CONST_Methodref, (6, 25))),
+                (3, (jt.CONST_Methodref, (7, 26))),
+                (4, (jt.CONST_Fieldref, (6, 27))),
+                (5, (jt.CONST_Fieldref, (6, 28))),
+                (6, (jt.CONST_Class, 29)),
+                (7, (jt.CONST_Class, 30)),
+                (8, (jt.CONST_Utf8, 'DEFAULT_NAME')),
+                (9, (jt.CONST_Utf8, 'Ljava/lang/String;')),
+                (10, (jt.CONST_Utf8, u'ConstantValue')),
+                (11, (jt.CONST_Utf8, u'name')),
+                (12, (jt.CONST_Utf8, u'recent_name')),
+                (13, (jt.CONST_Utf8, u'<init>')),
+                (14, (jt.CONST_Utf8, u'()V')),
+                (15, (jt.CONST_Utf8, u'Code')),
+                (16, (jt.CONST_Utf8, u'LineNumberTable')),
+                (17, (jt.CONST_Utf8, u'(Ljava/lang/String;)V')),
+                (18, (jt.CONST_Utf8, u'getName')),
+                (19, (jt.CONST_Utf8, u'()Ljava/lang/String;')),
+                (20, (jt.CONST_Utf8, u'getRecentName')),
+                (21, (jt.CONST_Utf8, u'<clinit>')),
+                (22, (jt.CONST_Utf8, u'SourceFile')),
+                (23, (jt.CONST_Utf8, u'Sample1.java')),
+                (24, (jt.CONST_Utf8, u'Daphne')),
+                (25, (jt.CONST_NameAndType, (13, 17))),
+                (26, (jt.CONST_NameAndType, (13, 14))),
+                (27, (jt.CONST_NameAndType, (11, 9))),
+                (28, (jt.CONST_NameAndType, (12, 9))),
+                (29, (jt.CONST_Utf8, u'Sample1')),
+                (30, (jt.CONST_Utf8, u'java/lang/Object')) )
+
+        col = tuple(enumerate(ci.cpool.consts))
+
+        self.assertEqual(col, exp)
+
+
+    def test_field_name(self):
         ci = load("Sample1")
         fi = ci.get_field_by_name("name")
 
@@ -127,8 +164,10 @@ class ClassfileTest(TestCase):
         self.assertFalse(fi.is_deprecated())
         self.assertFalse(fi.is_method)
 
+        self.assertEqual(fi.deref_constantvalue(), None)
 
-    def test_sample1_field_recent_name(self):
+
+    def test_field_recent_name(self):
         ci = load("Sample1")
         fi = ci.get_field_by_name("recent_name")
 
@@ -162,8 +201,10 @@ class ClassfileTest(TestCase):
         self.assertFalse(fi.is_deprecated())
         self.assertFalse(fi.is_method)
 
+        self.assertEqual(fi.deref_constantvalue(), None)
 
-    def test_sample1_field_default_name(self):
+
+    def test_field_default_name(self):
         ci = load("Sample1")
         fi = ci.get_field_by_name("DEFAULT_NAME")
 
@@ -197,18 +238,20 @@ class ClassfileTest(TestCase):
         self.assertFalse(fi.is_deprecated())
         self.assertFalse(fi.is_method)
 
-        self.assertEqual(fi.deref_constantvalue(),
-                         "Daphne")
+        self.assertEqual(fi.deref_constantvalue(), "Daphne")
 
 
-    def test_sample1_method_init(self):
+    def test_method_init(self):
         ci = load("Sample1")
 
         mis = list(ci.get_methods_by_name("<init>"))
         self.assertEqual(len(mis), 2)
 
         mi0 = ci.get_method("<init>")
+        self.assertEqual(type(mi0), jt.JavaMemberInfo)
+
         mi1 = ci.get_method("<init>", ["Ljava/lang/String;"])
+        self.assertEqual(type(mi1), jt.JavaMemberInfo)
 
         self.assertEqual(mis, [mi0, mi1])
 
@@ -267,6 +310,137 @@ class ClassfileTest(TestCase):
         self.assertFalse(mi1.is_module())
         self.assertFalse(mi1.is_deprecated())
         self.assertTrue(mi1.is_method)
+
+
+    def test_method_get_name(self):
+        ci = load("Sample1")
+        mi = ci.get_method("getName")
+
+        self.assertEqual(type(mi), jt.JavaMemberInfo)
+
+        self.assertEqual(mi.get_name(), "getName")
+        self.assertEqual(mi.get_type_descriptor(), "Ljava/lang/String;")
+        self.assertEqual(mi.get_descriptor(), "()Ljava/lang/String;")
+        self.assertEqual(mi.get_identifier(), "getName()")
+        self.assertEqual(mi.pretty_type(), "java.lang.String")
+        self.assertEqual(mi.pretty_descriptor(),
+                         "public java.lang.String getName()")
+        self.assertEqual(mi.pretty_identifier(),
+                         "getName():java.lang.String")
+
+        self.assertTrue(mi.is_public())
+        self.assertFalse(mi.is_private())
+        self.assertFalse(mi.is_protected())
+        self.assertFalse(mi.is_static())
+        self.assertFalse(mi.is_final())
+        self.assertFalse(mi.is_synchronized())
+        self.assertFalse(mi.is_native())
+        self.assertFalse(mi.is_abstract())
+        self.assertFalse(mi.is_strict())
+        self.assertFalse(mi.is_volatile())
+        self.assertFalse(mi.is_transient())
+        self.assertFalse(mi.is_bridge())
+        self.assertFalse(mi.is_varargs())
+        self.assertFalse(mi.is_synthetic())
+        self.assertFalse(mi.is_enum())
+        self.assertFalse(mi.is_module())
+        self.assertFalse(mi.is_deprecated())
+        self.assertTrue(mi.is_method)
+
+
+    def test_method_code_name(self):
+        ci = load("Sample1")
+        mi = ci.get_method("getName")
+        code = mi.get_code()
+
+        self.assertEqual(type(code), jt.JavaCodeInfo)
+
+        lnt = code.get_linenumbertable()
+        exp = ((0, 18), )
+
+        self.assertEqual(lnt, exp)
+
+        lnt = code.get_relativelinenumbertable()
+        exp = ((0, 0), )
+
+        self.assertEqual(lnt, exp)
+
+        dis = tuple(code.disassemble())
+
+        exp = ((0, op.OP_aload_0, ()),
+               (1, op.OP_getfield, (4,)),
+               (4, op.OP_areturn, ()))
+
+        self.assertEqual(dis, exp)
+
+        # this is the field loaded via OP_getfield
+        self.assertEqual(ci.cpool.pretty_deref_const(4),
+                         "Sample1.name:java.lang.String")
+
+
+    def test_method_get_recent_name(self):
+        ci = load("Sample1")
+        mi = ci.get_method("getRecentName")
+
+        self.assertEqual(type(mi), jt.JavaMemberInfo)
+
+        self.assertEqual(mi.get_name(), "getRecentName")
+        self.assertEqual(mi.get_type_descriptor(), "Ljava/lang/String;")
+        self.assertEqual(mi.get_descriptor(), "()Ljava/lang/String;")
+        self.assertEqual(mi.get_identifier(), "getRecentName()")
+        self.assertEqual(mi.pretty_type(), "java.lang.String")
+        self.assertEqual(mi.pretty_descriptor(),
+                         "public static java.lang.String getRecentName()")
+        self.assertEqual(mi.pretty_identifier(),
+                         "getRecentName():java.lang.String")
+
+        self.assertTrue(mi.is_public())
+        self.assertFalse(mi.is_private())
+        self.assertFalse(mi.is_protected())
+        self.assertTrue(mi.is_static())
+        self.assertFalse(mi.is_final())
+        self.assertFalse(mi.is_synchronized())
+        self.assertFalse(mi.is_native())
+        self.assertFalse(mi.is_abstract())
+        self.assertFalse(mi.is_strict())
+        self.assertFalse(mi.is_volatile())
+        self.assertFalse(mi.is_transient())
+        self.assertFalse(mi.is_bridge())
+        self.assertFalse(mi.is_varargs())
+        self.assertFalse(mi.is_synthetic())
+        self.assertFalse(mi.is_enum())
+        self.assertFalse(mi.is_module())
+        self.assertFalse(mi.is_deprecated())
+        self.assertTrue(mi.is_method)
+
+
+    def test_method_code_recent_name(self):
+        ci = load("Sample1")
+        mi = ci.get_method("getRecentName")
+        code = mi.get_code()
+
+        self.assertEqual(type(code), jt.JavaCodeInfo)
+
+        lnt = code.get_linenumbertable()
+        exp = ((0, 22), )
+
+        self.assertEqual(lnt, exp)
+
+        lnt = code.get_relativelinenumbertable()
+        exp = ((0, 0), )
+
+        self.assertEqual(lnt, exp)
+
+        dis = tuple(code.disassemble())
+
+        exp = ((0, op.OP_getstatic, (5,)),
+               (3, op.OP_areturn, ()))
+
+        self.assertEqual(dis, exp)
+
+        # this is the static field loaded via OP_getstatic
+        self.assertEqual(ci.cpool.pretty_deref_const(5),
+                         "Sample1.recent_name:java.lang.String")
 
 
 #
