00a48a097594ca7032f5e5e271c87dac3280cdca
Christopher O'Brien <obriencj@gmail.com>
2014-04-02 13:07:22 -0400

    Sample2 for more unit testing

---
+++ tests/data/Sample2I.java
@@ -0,0 +1,8 @@
+public interface Sample2I {
+    public boolean getBoolean(boolean b);
+    public byte getByte(byte b);
+    public char getChar(char c);
+    public int getInt(int i);
+    public long getLong(long l);
+    public Sample2I getSample();
+}
