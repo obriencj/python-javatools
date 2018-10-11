fffe3da7fc94828ee9d5a303be29fcac5c542d51
Konstantin Shemyak <konstantin@shemyak.com>
2018-10-11 23:24:25 +0300

    Manifest stored internally as Unicode, as it should be
    Manifest is loaded from text file only with parse_file(), not with open().
    Added test for modified lines, which were previously not covered.
    Py3-specific code is not added yet, this is a Py2-only internal improvement.

--- javatools/manifest.py
+++ javatools/manifest.py
@@ -338,11 +338,12 @@ class Manifest(ManifestSection):
 
     def parse_file(self, filename):
         """
-        Parse the given file, and attempt to detect the line separator.
+        Load self from the file, such as "MANIFEST.MF" or "SIGNATURE.SF".
+        :param filename: contains UTF-8 encoded manifest
         """
 
-        with open(filename, "r", _BUFFERING) as stream:
-            self.parse(stream)
+        with open(filename, "rb", _BUFFERING) as stream:
+            self.parse(stream.read())
 
 
     def load_from_jar(self, jarfile):
@@ -353,17 +354,17 @@ class Manifest(ManifestSection):
                 raise MissingManifestError(
                     "META-INF/MANIFEST.MF not found in %s" % jarfile)
             data = jar.read("META-INF/MANIFEST.MF")
-            if not isinstance(data, str):       # Py3
-                data = data.decode('utf-8')
             self.parse(data)
 
 
     def parse(self, data):
         """
-        populate instance with values and sub-sections from data in a
-        stream, string, or buffer
+        populate instance with values and sub-sections
+        :param data: UTF-8 encoded manifest
+        :type data: bytes
         """
 
+        data = data.decode('utf-8')
         self.linesep = detect_linesep(data)
 
         # the first section is the main one for the manifest. It's
@@ -680,17 +681,10 @@ def b64_encoded_digest(data, algorithm):
 
 
 def detect_linesep(data):
-    if isinstance(data, (str, buffer)):
-        data = BytesIO(data)
-
-    offset = data.tell()
-    line = data.readline()
-    data.seek(offset)
-
-    if line[-2:] == "\r\n":
-        return "\r\n"
-    else:
-        return line[-1]
+    """
+    :type data: unicode in Py2, str in Py3
+    """
+    return "\r\n" if "\r\n" in data else "\n"
 
 
 def parse_sections(data):
@@ -704,20 +698,18 @@ def parse_sections(data):
     present if the value is long enough to require line continuations
     in which case simply concatenate the vals together to get the full
     value.
+    :type data: unicode in Py2, str in Py3
     """
 
     if not data:
         return
 
-    if isinstance(data, (str, buffer)):
-        data = BytesIO(data)
-
     # our current section
     curr = None
 
-    for lineno, line in enumerate(data):
+    for lineno, line in enumerate(data.splitlines()):
         # Clean up the line
-        cleanline = line.splitlines()[0].replace('\x00', '')
+        cleanline = line.replace('\x00', '')
 
         if not cleanline:
             # blank line means end of current section (if any)
